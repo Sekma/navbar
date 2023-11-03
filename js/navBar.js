25f87fb3c9c3420e77e7953716e2787b9582670e
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2023-11-03 11:45:40 +0100

    first commit

--- js/navBar.js
+++ js/navBar.js
@@ -1,9 +1,20 @@
-$("#accueil").hover(function () {
+$(document).ready(function () {
+    $("#service").hover(function () {
         // over
         $("#menu-scroll").addClass('menu-scroll-active');
-        console.log("hello")
     }, function () {
         // out
         $("#menu-scroll").removeClass('menu-scroll-active');
     }
 );
+
+
+    $('#menu-scroll').hover(function () {
+            // over
+            $("#menu-scroll").addClass('menu-scroll-active');
+        }, function () {
+            // out
+            $("#menu-scroll").removeClass('menu-scroll-active');
+        }
+    );
+});
